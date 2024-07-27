d6e392eac87bcefd386c3e9cffe201bb2a12fcd5
Fish Nat <studyestudiante@gmail.com>
2024-07-26 20:04:52 -0400

    acpecto visual de la app en el titulo

---
+++ Actividades/cachipun.py
@@ -0,0 +1,20 @@
+# EL famoso juego de cachipun (piedra papel o tijera)
+
+
+
+# Importación de galería
+import random
+
+score = 0
+cpu_score = 0  
+
+# Aspecto visual de la aplicación en el título
+print("")
+print("")
+print("")
+
+print('******************************************************************')
+print('******************  Cachipun on Python  **************************')
+print('******************************************************************')
+print("")
+
